517734d36f339df64310ed8e56eb52ed82e00a4d
alejandro2Lopez <ale1jandro.lopez@gmail.com>
2023-12-05 08:08:36 -0600

    Configuración de JavaFX en Java with Ant

---
+++ src/pruebafx/PruebaFX.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class PruebaFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Prueba");
+
+        Button btn = new Button();
+        btn.setText("¡Haz clic!");
+        btn.setOnAction(e -> System.out.println("Hola Mundo"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+
+        primaryStage.show();
+    }
+}
